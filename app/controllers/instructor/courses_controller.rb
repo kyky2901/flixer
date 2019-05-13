1d9649fa46fb18840ed58ecab7ff098b477e1cf6
Kylie <kylie.rae.slattery@gmail.com>
2019-05-13 22:04:26 +0000

    routed top nav logo

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,9 +1,11 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+
   def new
     @course = Course.new
   end
-    def create
+
+  def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
       redirect_to instructor_course_path(@course)
